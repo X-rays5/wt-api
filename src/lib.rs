25c2686708515e1f299cf5bf1676d84beef0c16c
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-05-14 21:44:26 +0200

    Fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     router
         .get("/", |_,_| {Response::redirect_with_status(Url::parse("https://github.com/X-rays5/wt-api").unwrap(), 301)})
         .get_async("/v1/vehicles/:country/:category", v1::vehicles::get::country_specific)
-        .get_async("/v1/countries/", v1::countries::get::countries)
+        .get_async("/v1/countries", v1::countries::get::countries)
         .get_async("/v1/countries/have/:category", v1::countries::get::have_category)
         .get_async("/v1/categories", v1::categories::get::get_categories)
         .get_async("/v1/categories/has/:category/:country", v1::categories::get::country_has_categories)
